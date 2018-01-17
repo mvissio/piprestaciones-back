605dbac57b4aa2639e59ca96563b424a177fa612
MATIAS-PC\Matías <mati.monaco.91@gmail.com>
2018-01-17 19:33:20 -0300

    Vista de Carga de aplicacion

--- PiPiPrestaciones/Models/Aplicacion.cs
+++ PiPiPrestaciones/Models/Aplicacion.cs
@@ -35,37 +35,85 @@ namespace PiPiPrestaciones.Models
         [Display(Name = "CreateBy")]
         public string CreateBy { get; set; }
 
-
-
     }
 
 
     public class AplicacionCreateView {
 
-        [Key]
-        [Column("AplicacionId")]
+        [Key]  
         public int AplicacionId { get; set; }
 
-        [Column("NombreAplicacion")]
-        [Display(Name = "Nombre Aplicación")]
-        public string NombreAplicacion { get; set; }
+      
+        [Display(Name = "Título")]
+        public string Titulo { get; set; }
 
-        [Column("VersionId")]
+        [Display(Name = "App independiente")]     
+        public bool AppIndependiente { get; set; }
+
+        public string Idioma { get; set; }
+
+        public bool LenguajePorDefecto { get; set; }
+
+        public string ClaveApp { get; set; }
+
+        public string AdminUser { get; set; }
+
+        [DataType(DataType.Password)]
+        public string AdminPasword { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string Canal { get; set; }
+
+        public string UrlImagenN { get; set; }
+
+        public string UrlImagen2 { get; set; }
+
+        public string UrlImagen1 { get; set; }
+
+        public string HashTagTwiter { get; set; }
+
+      
+
+
         [Display(Name = "VersionId")]
         public int? VersionId { get; set; }
 
-        [Column("LastModification")]
-        [Display(Name = "Última modiicación")]
+       
+      
+        [Display(AutoGenerateField = false,Name = "Última modiicación")]
         public DateTime? LastModification { get; set; }
 
-        [Column("CreateAt")]
-        [Display(Name = "CreateAt")]
+       
+        [Display(AutoGenerateField = false,Name = "CreateAt")]
         public DateTime CreateAt { get; set; }
 
-        [Column("CreateBy")]
-        [Display(Name = "CreateBy")]
+      
+        [Display(AutoGenerateField = false,Name = "CreateBy")]
         public string CreateBy { get; set; }
 
+
+
+    }
+
+
+    public class PaletaColores
+    {
+        public string FondoGeneral { get; set; }
+
+        public string LetrasGeneral { get; set; }
+
+        public string FondoBotones { get; set; }
+
+        public string LetrasBotones { get; set; }
+
+        public string CompartirFondo { get; set; }
+
+        public string CompartirTexto { get; set; }
+
+        public string FondoTabs { get; set; }
+
+        public string LetrasTabs { get; set; }
 
 
     }

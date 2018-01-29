3161be56843ec7b65fea3dcba79cfbd46efc10d6
MATIAS-PC\Matías <mati.monaco.91@gmail.com>
2018-01-29 17:15:37 -0300

    Se corrigió detalles en index y create aplicación

---
+++ PiPiPrestaciones/Models/Type.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PiPiPrestaciones.Models
+{
+    public class Type
+    {
+        [Key]
+        public int TypeId { get; set; }
+
+        public string TypeName { get; set; }
+    }
+}
